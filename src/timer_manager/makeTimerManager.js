2c526b4c5196858ba5a177e02e0b52feab857fd4
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-06-10 05:46:47 +0600

    complete the timer manager module

--- src/timer_manager/makeTimerManager.js
+++ src/timer_manager/makeTimerManager.js
@@ -5,6 +5,9 @@ module.exports = function makeTimerManager({
   Speaker,
   configManager,
 }) {
+  /**
+   * A singleton that manages CRUD operations on timers
+   * */
   return class TimerManager {
     #timerLogger;
     #currentTimer;
@@ -16,6 +19,8 @@ module.exports = function makeTimerManager({
       Speaker.on(this.#beepDuration);
       await this.#timerLogger.log(timerInfo);
     };
+
+    #timerCommands = Object.freeze(["END", "INFO", "RESET", "START", "PAUSE"]);
 
     #isInitiated = false;
 
@@ -37,52 +42,82 @@ module.exports = function makeTimerManager({
       this.#isInitiated = true;
     }
 
+    /**
+     * @returns {{ success: true } | {success: false, message: string}}
+     * */
     async execute(commandObject) {
       try {
-        await this.#execute(commandObject);
+        const result = await this.#execute(commandObject);
+        return result ? result : { success: true };
       } catch (ex) {
-        console.error(ex);
         // @TODO handle error
+        // console.error(ex);
+        return { success: false, message: ex.message };
       }
     }
 
     async #execute(commandObject) {
-      // @TODO do validation
-      const { commandName, commandArg } = commandObject;
+      const { command, arg } = commandObject;
 
-      switch (commandName) {
+      const isTimerCommandWithoutArg =
+        // if commandName is a timer command AND
+        // (if commandName is not "START" with a timer name)
+        this.#timerCommands.includes(command) && !(command === "START" && arg);
+
+      if (isTimerCommandWithoutArg) {
+        return this.#currentTimer
+          ? this.#currentTimer[command.toLowerCase()]()
+          : { success: false, message: "No timer exists." };
+      }
+
+      switch (command) {
         case "CREATE":
-          this.#createTimer(commandArg);
-          break;
-        case "RESET":
-          break;
+          return this.#createTimer(arg);
         case "START":
-          this.#startTimer(commandArg);
-          break;
-        case "PAUSE":
-          break;
-        case "END":
-          break;
+          return this.#startTimer(arg);
         case "SAVE":
+          await this.#saveTimer();
           break;
         case "UPDATE_CONFIG":
           await this.#updateConfig();
           break;
+        case "LIST_SAVED_TIMERS":
+          return this.#listSavedTimers();
+        case "DELETE_SAVED_TIMER":
+          await configManager.deleteSavedTimer(arg);
+          await this.#updateConfig();
+          break;
         default:
-          throw new EPP(`Invalid Command "${commandName}"`);
+          return {
+            success: false,
+            message: `Invalid command: "${command}"`,
+          };
       }
     }
 
+    async #saveTimer() {
+      if (this.#currentTimer)
+        await configManager.saveTimer(
+          this.#getBasicTimerInfo(this.#currentTimer)
+        );
+      else return { success: false, message: "No timer exists." };
+    }
+
+    #listSavedTimers() {
+      return Object.values(this.#savedTimers).map(this.#getBasicTimerInfo);
+    }
+
+    #getBasicTimerInfo = (timer) => {
+      const { name, duration, description } = timer.info();
+      return { name, duration, description };
+    };
+
     #startTimer(timerName) {
-      if (timerName) {
-        if (timerName in this.#savedTimers)
-          this.#currentTimer = this.#savedTimers[timerName];
-        else throw new EPP(`No saved timer with name: "${timerName}"`);
-      }
+      if (timerName in this.#savedTimers)
+        this.#currentTimer = this.#savedTimers[timerName];
+      else throw new EPP(`No saved timer with name: "${timerName}"`);
 
-      if (!this.#currentTimer) throw new EPP(`No timer exists.`);
-
-      this.#currentTimer.start();
+      return this.#currentTimer.start();
     }
 
     #createTimer(timerInfo) {
