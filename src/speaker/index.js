abeb770ab160d475cf24a2e7e3ca500a7d0e73de
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-06-11 22:58:30 +0600

    refactor speaker module

--- src/speaker/index.js
+++ src/speaker/index.js
@@ -1,41 +1,73 @@
 const { spawn } = require("child_process");
 const { makeEnum } = require("../util");
 
-const MPLAYER_TOGGLE_PAUSE_CMD = "p\n";
-
-const player = spawn(
-  "mplayer",
-  ["-nogui", "-slave", "-really-quiet", "-loop", "0", "alarm_beep.mp3"],
-  {
-    cwd: __dirname,
-  }
-);
-
-const toggleSpeaker = () => player.stdin.write(MPLAYER_TOGGLE_PAUSE_CMD);
-
-// Immediately turn off the speaker after spawning it.
-toggleSpeaker();
-
+const M_PLAYER_TOGGLE_PAUSE_KEY = "p\n";
 const SPEAKER_STATES = makeEnum({ keys: ["OFF", "ON"], startingValue: 0 });
 
-let speakerState = SPEAKER_STATES.OFF;
+class Speaker {
+  #state = SPEAKER_STATES.OFF;
+  #player;
+  #onCallback;
+  #offCallback;
+  #offTimeoutId;
 
-function on(timeout) {
-  if (speakerState === SPEAKER_STATES.ON) return;
+  static #instance;
+  constructor({ onCallback = () => {}, offCallback = () => {} } = {}) {
+    if (Speaker.#instance) return Speaker.#instance;
+    Speaker.#instance = this;
 
-  speakerState = SPEAKER_STATES.ON;
-  toggleSpeaker();
+    this.#player = spawn(
+      "mplayer",
+      ["-nogui", "-slave", "-really-quiet", "-loop", "0", "alarm_beep.mp3"],
+      {
+        cwd: __dirname,
+      }
+    );
 
-  if (timeout) setTimeout(off, timeout);
+    // immediately turn off the player
+    this.#togglePlayer();
+
+    this.#onCallback = onCallback;
+    this.#offCallback = offCallback;
+  }
+
+  on(timeout) {
+    if (this.#state === SPEAKER_STATES.ON) return;
+
+    this.#togglePlayer();
+    this.#state = SPEAKER_STATES.ON;
+
+    this.#onCallback();
+    if (timeout) this.#offTimeoutId = setTimeout(() => this.off(), timeout);
+  }
+
+  off() {
+    if (this.#state === SPEAKER_STATES.OFF) return;
+
+    this.#togglePlayer();
+    this.#state = SPEAKER_STATES.OFF;
+
+    if (this.#offTimeoutId) clearTimeout(this.#offTimeoutId);
+    this.#offCallback();
+  }
+
+  #togglePlayer() {
+    this.#player.stdin.write(M_PLAYER_TOGGLE_PAUSE_KEY);
+  }
+
+  set onCallback(func) {
+    this.#assertValueIsFunction({ value: func, name: "onCallback" });
+    this.#onCallback = func;
+  }
+  set offCallback(func) {
+    this.#assertValueIsFunction({ value: func, name: "offCallback" });
+    this.#offCallback = func;
+  }
+
+  #assertValueIsFunction({ value, name }) {
+    if (typeof value !== "function")
+      throw new Error(`${name} must be a function.`);
+  }
 }
-
-function off() {
-  if (speakerState === SPEAKER_STATES.OFF) return;
-
-  speakerState = SPEAKER_STATES.OFF;
-  toggleSpeaker();
-}
-
-const Speaker = { on, off };
 
 module.exports = Speaker;
