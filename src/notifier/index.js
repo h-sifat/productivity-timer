65ddae77e7392bf5dcbf7e040abaf65727eb0be9
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-06-16 10:23:42 +0600

    feat(notifier): add cross-platform support
    1. Added the "node-notifier" package from npm to support cross-platform
    functionality.
    1. Also added "mem-fs" to test some modulses.

--- src/notifier/index.js
+++ src/notifier/index.js
@@ -1,8 +1,14 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const nodeNotifier = require("node-notifier");
+const NOTIFICATION_TITLE = "Pomodoro Timer";
 
-async function notify(text) {
-  await exec(`notify-send '${text}'`);
+async function notify(arg) {
+  if (typeof arg === "string")
+    nodeNotifier.notify({
+      title: NOTIFICATION_TITLE,
+      message: arg,
+    });
+  else if (typeof arg === "object" && arg !== null) nodeNotifier.notify(arg);
+  else throw new Error(`Invalid argument to notifier.`);
 }
 
 module.exports = notify;
