3ce1b80187cdf5523e1e5e51aa8cb1fe4e65989c
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-09-04 10:49:32 +0600

    feat: delete all modules and migrate to Typescript
    Sorry, I really don't know if this commit should be in the feat
    category! I couldn't any suitable category.
    In this commit I've deleted all the modules in the src and tests folder
    because I want to rewrite this application with a completely different
    architecture. InshaAllah it'll include a lot of new features and
    capabilities that was not intended in the previous version.

--- src/util/request/index.js
+++
@@ -1,6 +0,0 @@
-const net = require("net");
-const buildRequestToIPCServer = require("./buildRequestToIPCServer");
-
-const requestToIPCServer = buildRequestToIPCServer({ net });
-
-module.exports = requestToIPCServer;
