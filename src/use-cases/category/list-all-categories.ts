5f151412123b4b993b91d6f257cb4938aac97a23
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-09-10 11:38:45 +0600

    feat(use-cases/category): add list-all-categories

---
+++ src/use-cases/category/list-all-categories.ts
@@ -0,0 +1,45 @@
+import type CategoryDatabase from "./interfaces/category-db";
+import type { CategoryInterface } from "entities/category/category";
+import type { MakeCategoryIfNotCorrupted } from "./interfaces/util";
+
+import EPP from "common/util/epp";
+import Category from "entities/category";
+
+interface MakeListAllCategories_Argument {
+  db: CategoryDatabase;
+  makeCategoryIfNotCorrupted: MakeCategoryIfNotCorrupted;
+}
+
+export default function makeListAllCategories(
+  arg: MakeListAllCategories_Argument
+) {
+  const { db, makeCategoryIfNotCorrupted } = arg;
+
+  interface ListAllCategories_Result {
+    corruptionError: EPP[];
+    categories: CategoryInterface[];
+  }
+
+  return async function listAllCategories(): Promise<ListAllCategories_Result> {
+    const allCategoryRecords = await db.findAll();
+
+    const result: ListAllCategories_Result = {
+      categories: [],
+      corruptionError: [],
+    };
+
+    for (const categoryInfo of allCategoryRecords)
+      try {
+        const childCategory = makeCategoryIfNotCorrupted({
+          CategoryClass: Category,
+          categoryRecord: categoryInfo,
+        });
+
+        result.categories.push(childCategory);
+      } catch (ex: any) {
+        result.corruptionError.push(ex);
+      }
+
+    return result;
+  };
+}
---
+++ tests/src/use-cases/category/list-all-categories.test.ts
@@ -0,0 +1,41 @@
+import Category from "entities/category";
+import { getCategoryDatabase } from "fixtures/use-case/category-db";
+import makeListAllCategories from "use-cases/category/list-all-categories";
+import makeCategoryIfNotCorrupted from "use-cases/category/util";
+
+const db = getCategoryDatabase();
+
+const listAllCategories = makeListAllCategories({
+  db,
+  makeCategoryIfNotCorrupted: makeCategoryIfNotCorrupted,
+});
+
+afterEach(() => {
+  db._clearDb_();
+});
+
+describe("Functionality", () => {
+  it(`returns empty array if no category exists in db`, async () => {
+    const result = await listAllCategories();
+
+    expect(result).toEqual({
+      categories: [],
+      corruptionError: [],
+    });
+  });
+
+  it(`lists all the categories`, async () => {
+    const categoryRecord = new Category({ name: "study" }).toPlainObject();
+    {
+      const store = db._getStore_();
+      store[categoryRecord.id] = categoryRecord;
+    }
+
+    const { categories, corruptionError } = await listAllCategories();
+
+    expect(corruptionError).toHaveLength(0);
+    expect(categories).toHaveLength(1);
+
+    expect(categories.pop()?.toPlainObject()).toEqual(categoryRecord);
+  });
+});
