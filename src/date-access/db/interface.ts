bcfd5b378f0809a2d1c42ed0eaad06f90191172c
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-10-04 22:33:44 +0600

    feat(data-access/db): add sqlite database

---
+++ src/date-access/db/interface.ts
@@ -0,0 +1,77 @@
+export interface QueryMethodArguments {
+  close: undefined;
+  isOpen: undefined;
+  open: { path: string };
+  execute: { sql: string };
+  pragma: { command: string };
+  isPrepared: { name: string };
+  backup: { destination: string };
+  deletePrepared: { name: string };
+  prepare: { name: string; statement: string };
+  runPrepared: { name: string; statementArgs?: object };
+  executePrepared: { name: string; statementArgs?: object };
+}
+
+interface NonVoidReturnTypes {
+  isOpen: Promise<boolean>;
+  pragma: Promise<unknown>;
+  isPrepared: Promise<boolean>;
+  executePrepared: Promise<unknown[]>;
+}
+
+export type QueryMethodReturnType = Record<
+  keyof Omit<QueryMethodArguments, keyof NonVoidReturnTypes>,
+  Promise<void>
+> &
+  NonVoidReturnTypes;
+
+interface Command<Name extends keyof QueryMethodArguments, Argument> {
+  name: Name;
+  reject: Function;
+  resolve: Function;
+  argument: Argument;
+}
+
+type MakeCommands<
+  _QMA extends QueryMethodArguments,
+  methods extends keyof QueryMethodArguments
+> = {
+  [method in methods]: Command<method, _QMA[method]>;
+};
+
+export type Commands = MakeCommands<
+  QueryMethodArguments,
+  keyof QueryMethodArguments
+>;
+
+export interface DbSubprocessResponse {
+  result: any;
+  error: null | object;
+}
+
+type MakeCommandsForSubprocessDB<
+  _QMA extends QueryMethodArguments,
+  methods extends keyof QueryMethodArguments
+> = {
+  [method in methods]: { method: method; argument: _QMA[method] };
+}[methods];
+
+export type DbSubprocessCommands = MakeCommandsForSubprocessDB<
+  QueryMethodArguments,
+  keyof QueryMethodArguments
+>;
+
+export interface DbSubProcess {
+  send(command: DbSubprocessCommands): void;
+  kill(signal?: number | NodeJS.Signals | undefined): boolean;
+  on(event: "message", callbacK: (arg: DbSubprocessResponse) => void): void;
+
+  on(
+    event: "close",
+    callbacK: (code: number | null, signal: string | null) => void
+  ): void;
+
+  removeListener(event: "message" | "close", listener: Function): void;
+}
+
+export type MakeDbSubProcess = () => DbSubProcess;
---
+++ src/date-access/db/sqlite.js
@@ -0,0 +1,116 @@
+const Sqlite3 = require("better-sqlite3");
+
+module.exports = class __SqliteDb__ {
+  #db = null;
+  #preparedStatements = {};
+
+  open(arg) {
+    if (this.#db)
+      throw new EPP({
+        message: `The database is already open.`,
+        code: "DB_IS_ALREADY_OPEN",
+      });
+
+    const { path } = arg;
+    this.#db = new Sqlite3(path, { fileMustExist: true });
+  }
+
+  close() {
+    this.#assertDbIsOpen();
+
+    this.#db.close();
+    this.#db = null;
+    this.#preparedStatements = {};
+  }
+
+  pragma(arg) {
+    this.#assertDbIsOpen();
+
+    const { command: pragmaCommand } = arg;
+    return this.#db.pragma(pragmaCommand, { simple: true });
+  }
+
+  isPrepared(arg) {
+    this.#assertDbIsOpen();
+
+    const { name } = arg;
+    return name in this.#preparedStatements;
+  }
+
+  deletePrepared(arg) {
+    this.#assertDbIsOpen();
+
+    const { name } = arg;
+    delete this.#preparedStatements[name];
+
+    if (name in this.#preparedStatements)
+      throw new EPP({
+        code: "IE:COULD_NOT_DELETE_STMT",
+        message: `Internal Error: Couldn't delete prepared statement: "${name}"`,
+      });
+  }
+
+  prepare(arg) {
+    this.#assertDbIsOpen();
+
+    const { name, statement } = arg;
+    this.#preparedStatements[name] = this.#db.prepare(statement);
+  }
+
+  #executePreparedStatement(arg) {
+    const { name } = arg;
+
+    if (!(name in this.#preparedStatements))
+      throw new EPP({
+        code: "STMT_DOES_NOT_EXIST",
+        message: `No prepared statement found with name: "${name}"`,
+      });
+
+    {
+      const { method } = arg;
+
+      const { statementArgs = {} } = arg;
+      return this.#preparedStatements[name][method](statementArgs);
+    }
+  }
+
+  runPrepared(arg) {
+    this.#assertDbIsOpen();
+    return this.#executePreparedStatement({ method: "run", ...arg });
+  }
+
+  executePrepared(arg) {
+    this.#assertDbIsOpen();
+    return this.#executePreparedStatement({ method: "all", ...arg });
+  }
+
+  execute(arg) {
+    this.#assertDbIsOpen();
+    this.#db.exec(arg.sql);
+  }
+
+  backup(arg) {
+    this.#assertDbIsOpen();
+    return this.#db.backup(arg.destination);
+  }
+
+  isOpen() {
+    return !!this.#db;
+  }
+
+  #assertDbIsOpen() {
+    if (!this.#db)
+      throw new EPP({
+        code: "DB_NOT_INITIALIZED",
+        message:
+          "Database has not been initialized yet. Please open a database first.",
+      });
+  }
+};
+
+class EPP extends Error {
+  constructor({ message, code }) {
+    super(message);
+    this.code = code;
+  }
+}
