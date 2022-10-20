e919de2d9861e0d84119aed9a0872348d0d5cfa1
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-10-20 09:33:09 +0600

    refactor(tests/edit-category): use mocks
    Use mocks instead of a dummy database to test API calls are being made
    properly.

--- tests/src/use-cases/category/edit-category.test.ts
+++ tests/src/use-cases/category/edit-category.test.ts
@@ -1,15 +1,19 @@
 import getID from "src/data-access/id";
 import Category from "entities/category";
-import CategoryDatabase from "fixtures/use-case/category-db";
 import makeEditCategory from "use-cases/category/edit-category";
 
-const db = new CategoryDatabase();
+const db = Object.freeze({
+  findById: jest.fn(),
+  updateById: jest.fn(),
+});
+const dbMethodsCount = Object.keys(db).length;
+
 const Id = getID({ entity: "category" });
 
 const editCategory = makeEditCategory({ db, isValidId: Id.isValid });
 
 beforeEach(async () => {
-  await db.clearDb();
+  Object.values(db).forEach((method) => method.mockReset());
 });
 
 describe("Validation", () => {
@@ -17,7 +21,7 @@ describe("Validation", () => {
     const errorCode = "MISSING_ID";
 
     it(`throws ewc "${errorCode}" if id is missing`, async () => {
-      expect.assertions(1);
+      expect.assertions(1 + dbMethodsCount);
 
       try {
         // @ts-ignore
@@ -25,6 +29,9 @@ describe("Validation", () => {
       } catch (ex: any) {
         expect(ex.code).toBe(errorCode);
       }
+
+      for (const method of Object.values(db))
+        expect(method).not.toHaveBeenCalled();
     });
   }
 
@@ -32,7 +39,7 @@ describe("Validation", () => {
     const errorCode = "MISSING_CHANGES";
 
     it(`throws ewc "${errorCode}" if the "changes" object is missing`, async () => {
-      expect.assertions(1);
+      expect.assertions(1 + dbMethodsCount);
 
       try {
         // @ts-ignore
@@ -40,6 +47,9 @@ describe("Validation", () => {
       } catch (ex: any) {
         expect(ex.code).toBe(errorCode);
       }
+
+      for (const method of Object.values(db))
+        expect(method).not.toHaveBeenCalled();
     });
   }
 
@@ -47,7 +57,7 @@ describe("Validation", () => {
     const errorCode = "INVALID_ID";
 
     it(`it throws ewc "${errorCode}" if id is not valid`, async () => {
-      expect.assertions(2);
+      expect.assertions(2 + dbMethodsCount);
 
       const invalidId = "non_numeric_string";
       expect(Id.isValid(invalidId)).toBeFalsy();
@@ -57,6 +67,9 @@ describe("Validation", () => {
       } catch (ex: any) {
         expect(ex.code).toBe(errorCode);
       }
+
+      for (const method of Object.values(db))
+        expect(method).not.toHaveBeenCalled();
     });
   }
 
@@ -64,7 +77,10 @@ describe("Validation", () => {
     const errorCode = "NOT_FOUND";
 
     it(`throws ewc "${errorCode}" if the category with the given id doesn't exist`, async () => {
-      expect.assertions(1);
+      expect.assertions(3);
+
+      // meaning no category exists with the given id
+      db.findById.mockResolvedValueOnce(null);
 
       try {
         // currently the db is empty so no category should
@@ -73,6 +89,9 @@ describe("Validation", () => {
       } catch (ex: any) {
         expect(ex.code).toBe(errorCode);
       }
+
+      expect(db.findById).toHaveBeenCalledTimes(1);
+      expect(db.updateById).not.toHaveBeenCalled();
     });
   }
 
@@ -80,56 +99,75 @@ describe("Validation", () => {
     const errorCode = "PARENT_NOT_FOUND";
 
     it(`throws ewc "${errorCode}" if the category with the given id doesn't exist`, async () => {
-      expect.assertions(2);
+      expect.assertions(5);
 
       const category = Category.make({ name: "a" });
       const id = category.id;
+      const parentId = id + "1";
 
       // manually inserting a category in the db
-      await db.insert(category);
+      db.findById.mockResolvedValueOnce(category);
+      // the second call should be to find the parent
+      db.findById.mockResolvedValueOnce(null);
 
       expect(category.parentId).toBeNull();
 
       try {
-        // currently the db only has the category so a parent category
-        // with the id: (category.id + "1") does not exist
-        await editCategory({ id, changes: { parentId: id + "1" } });
+        // as the db.findById will return null in the second call, it should
+        // throw an error
+        await editCategory({ id, changes: { parentId } });
       } catch (ex: any) {
         expect(ex.code).toBe(errorCode);
       }
+
+      expect(db.updateById).not.toHaveBeenCalled();
+      expect(db.findById).toHaveBeenCalledTimes(2);
+      expect(db.findById.mock.calls).toEqual([[{ id }], [{ id: parentId }]]);
     });
   }
 });
 
 describe("Functionality", () => {
   it("edits a category if everything is valid", async () => {
-    const categoryBeforeEdit = {
+    const category = {
       ...Category.make({ name: "a" }),
-      createdAt: 100,
+      // createdAt: 100,
     };
 
-    expect(categoryBeforeEdit.description).toBeNull();
+    expect(category.description).toBeNull();
 
-    const id = categoryBeforeEdit.id;
+    const id = category.id;
 
-    await db.insert(categoryBeforeEdit);
+    // as if we're inserting the category in db
+    db.findById.mockResolvedValueOnce(category);
 
     const editedDescription = "description";
-    const editedName = categoryBeforeEdit.name + "_";
+    const editedName = category.name + "_";
 
-    const categoryAfterEdit = await editCategory({
+    const editedCategory = await editCategory({
       id,
       changes: { description: editedDescription, name: editedName },
     });
 
-    expect(categoryAfterEdit).toMatchObject({
+    expect(editedCategory).toMatchObject({
       id,
       name: editedName,
       description: editedDescription,
-      parentId: categoryBeforeEdit.parentId,
-      createdAt: categoryBeforeEdit.createdAt,
+      parentId: category.parentId,
+      createdAt: category.createdAt,
     });
 
-    expect(categoryBeforeEdit.hash).not.toBe(categoryAfterEdit.hash);
+    expect(category.hash).not.toBe(editedCategory.hash);
+
+    // as the changes doesn't edit the parentId, so it should not to lookup
+    // the parent  category thus calling the db.findById only once
+    expect(db.findById).toHaveBeenCalledTimes(1);
+    expect(db.findById).toHaveBeenCalledWith({ id: category.id });
+
+    expect(db.updateById).toHaveBeenCalledTimes(1);
+    expect(db.updateById).toHaveBeenCalledWith({
+      id: category.id,
+      edited: editedCategory,
+    });
   });
 });
