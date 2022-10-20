b2be45a92421811799b62531aaf40f94154b267c
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-10-20 11:47:23 +0600

    refactor(tests/get-project): use mocks for db

--- tests/src/use-cases/project/get-project.test.ts
+++ tests/src/use-cases/project/get-project.test.ts
@@ -1,14 +1,13 @@
-import Project from "entities/project";
 import { isValid as isValidId } from "common/util/id";
 import makeGetProject from "use-cases/project/get-project";
-import ProjectDatabase from "fixtures/use-case/project-db";
 
-const db = new ProjectDatabase();
-
+const db = Object.freeze({
+  findById: jest.fn(),
+});
 const getProject = makeGetProject({ db, isValidId });
 
-beforeEach(async () => {
-  await db.clearDb();
+beforeEach(() => {
+  Object.values(db).forEach((method) => method.mockReset());
 });
 
 describe("Validation", () => {
@@ -16,7 +15,7 @@ describe("Validation", () => {
     const errorCode = "INVALID_ID";
 
     it(`it throws ewc "${errorCode}" if id is not valid`, async () => {
-      expect.assertions(2);
+      expect.assertions(3);
 
       const invalidId = "non_numeric_string";
       expect(isValidId(invalidId)).toBeFalsy();
@@ -26,22 +25,22 @@ describe("Validation", () => {
       } catch (ex: any) {
         expect(ex.code).toBe(errorCode);
       }
+
+      expect(db.findById).not.toHaveBeenCalled();
     });
   }
 });
 
 describe("Functionality", () => {
-  it(`returns null if project doesn't exist`, async () => {
-    // db is empty
-    expect(await getProject({ id: "100" })).toBeNull();
-  });
-  it(`returns the project if it exists`, async () => {
-    const inserted = Project.make({ name: "Todo" });
+  it(`returns whatever the db.findById returns without validation`, async () => {
+    const id = "123";
+    const project = "todo";
 
-    // inserting manually
-    await db.insert(inserted);
+    db.findById.mockResolvedValueOnce(project);
 
-    const queriedProjectInfo = await getProject({ id: inserted.id });
-    expect(queriedProjectInfo).toEqual(inserted);
+    expect(await getProject({ id })).toEqual(project);
+
+    expect(db.findById).toHaveBeenCalledTimes(1);
+    expect(db.findById).toHaveBeenCalledWith({ id });
   });
 });
