80518fc080e985b852a8e24eba2d2dfd1bfc65a4
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-10-22 11:48:43 +0600

    feat(use-cases/category): add service factory

---
+++ src/use-cases/category/index.ts
@@ -0,0 +1,34 @@
+import getID from "data-access/id";
+import makeAddCategory from "./add-category";
+import makeEditCategory from "./edit-category";
+import makeGetCategoryMaxId from "./get-max-id";
+import makeListCategories from "./list-categories";
+import makeRemoveCategory from "./remove-category";
+import makeListSubCategories from "./list-sub-categories";
+import makeListParentCategories from "./list-parent-categories";
+import type CategoryDatabaseInterface from "use-cases/interfaces/category-db";
+import { CategoryServiceInterface } from "use-cases/interfaces/category-service";
+
+export interface MakeCategoryService_Argument {
+  database: CategoryDatabaseInterface;
+}
+
+export default function makeCategoryService(
+  builderArg: MakeCategoryService_Argument
+): CategoryServiceInterface {
+  const { database: db } = builderArg;
+  const Id = getID({ entity: "category" });
+  const isValidId = Id.isValid;
+
+  const categoryService = Object.freeze({
+    addCategory: makeAddCategory({ db }),
+    getMaxId: makeGetCategoryMaxId({ db }),
+    listCategories: makeListCategories({ db }),
+    editCategory: makeEditCategory({ db, isValidId }),
+    listSubCategories: makeListSubCategories({ db, Id }),
+    removeCategory: makeRemoveCategory({ db, isValidId }),
+    listParentCategories: makeListParentCategories({ db, isValidId }),
+  });
+
+  return categoryService;
+}
