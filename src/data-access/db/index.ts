3b8e0087411205313c6cc77beee162cfadc8ec40
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-15 22:05:41 +0600

    fix(data-access): trying to respawn subprocess blidly
    If the subprocess module gets deleted while the app is running and if it
    crashes, then the app would blindly try to respawn the subprocess. Fixed
    it by adding a condition to check the existence of module before forking
    a child process.

--- src/data-access/db/index.ts
+++ src/data-access/db/index.ts
@@ -1,12 +1,21 @@
+import { existsSync } from "fs";
+import EPP from "common/util/epp";
 import { fork } from "child_process";
 import { getConfig } from "src/config";
 import { DbSubProcess } from "./interface";
 
-const config = getConfig();
-
 export function makeDbSubProcess(): DbSubProcess {
-  return fork(config.DB_SUB_PROCESS_MODULE_PATH, {
-    stdio: "inherit",
-    cwd: process.cwd(),
+  // it's important to get the config dynamically
+  const modulePath = getConfig().DB_SUB_PROCESS_MODULE_PATH;
+
+  if (existsSync(modulePath))
+    return fork(modulePath, {
+      stdio: "inherit",
+      cwd: process.cwd(),
+    });
+
+  throw new EPP({
+    code: "DB_SUBPROCESS_MODULE_NOT_FOUND",
+    message: `The subprocess module ("${modulePath}") has been deleted.`,
   });
 }
