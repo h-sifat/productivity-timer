a42fba63bf362d4b24160e7e1f3b27752f2117a4
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-17 08:19:17 +0600

    feat: add configuration file

--- src/configure.ts
+++ src/configure.ts
@@ -3,6 +3,7 @@ import EPP from "common/util/epp";
 import { promises as fsp } from "fs";
 import { accessPath } from "common/util/fs";
 import { getConfig, modifyConfig } from "./config";
+import { FileConfigInterface, validateFileConfig } from "./config/util";
 
 export interface configureApplication_Argument {
   log(message: string): Promise<void>;
@@ -10,44 +11,97 @@ export interface configureApplication_Argument {
 
 export async function configureApplication(arg: configureApplication_Argument) {
   const { log } = arg;
-  const config = getConfig();
+
+  {
+    const initialConfig = getConfig();
+
+    let fileConfig: FileConfigInterface;
+
+    {
+      await log("Looking for configuration file...");
+      const { exists, hasPermissions } = await accessPath({
+        path: initialConfig.CONFIG_FILE_PATH,
+        permissions: { read: true, write: true },
+      });
+
+      if (exists && !hasPermissions)
+        throwNotAccessibleError(initialConfig.CONFIG_FILE_PATH);
+
+      if (exists) {
+        await log("\tFound config file.");
+        let unValidatedConfig: any;
+        try {
+          const configJSON = await fsp.readFile(
+            initialConfig.CONFIG_FILE_PATH,
+            "utf8"
+          );
+
+          unValidatedConfig = JSON.parse(configJSON);
+        } catch (ex) {
+          throw new Error(`\tCould not read/parse config file.`);
+        }
+
+        fileConfig = validateFileConfig(unValidatedConfig);
+      } else {
+        await log(`\tConfig file not found. Using default configurations.`);
+        fileConfig = validateFileConfig({}); // will return default values
+
+        await fsp.writeFile(
+          initialConfig.CONFIG_FILE_PATH,
+          JSON.stringify(fileConfig),
+          "utf8"
+        );
+      }
+    }
+
+    // the module name is coming from the webpack.config.js file
+    const DB_SUB_PROCESS_MODULE_PATH = path.join(
+      __dirname,
+      "./db_subprocess.js"
+    );
+
+    const DB_PATH = path.join(fileConfig.DATA_DIR, initialConfig.DB_FILE_NAME);
+    modifyConfig({
+      changes: { ...fileConfig, DB_PATH, DB_SUB_PROCESS_MODULE_PATH },
+    });
+  }
+
+  const updatedConfig = getConfig();
 
   await log("Making sure that all data directory exists.");
-  for (const dir of [config.DATA_DIR, config.DB_BACKUP_DIR]) {
+  for (const dir of [updatedConfig.DATA_DIR, updatedConfig.DB_BACKUP_DIR]) {
     const { exists, hasPermissions } = await accessPath({
       path: dir,
       permissions: { read: true, write: true },
     });
 
     if (exists && !hasPermissions) throwNotAccessibleError(dir);
-    if (exists && (await fsp.stat(dir)).isDirectory()) continue;
+    if (exists && (await fsp.stat(dir)).isDirectory()) {
+      await log(`\tExists: ${dir}`);
+      continue;
+    }
 
-    await log(`Creating directory: "${dir}"`);
+    await log(`\tNot found: ${dir}.\n\tCreating directory: "${dir}"`);
     await fsp.mkdir(dir, { recursive: true });
   }
 
-  const DB_PATH = path.join(config.DATA_DIR, config.DB_FILE_NAME);
+  const DB_PATH = path.join(updatedConfig.DATA_DIR, updatedConfig.DB_FILE_NAME);
   const DB_BACKUP_PATH = path.join(
-    config.DB_BACKUP_DIR,
-    config.DB_BACKUP_FILE_NAME
+    updatedConfig.DB_BACKUP_DIR,
+    updatedConfig.DB_BACKUP_FILE_NAME
   );
 
   await log("Checking database file.");
 
-  (async () => {
-    {
-      const { exists, hasPermissions } = await accessPath({
-        path: DB_PATH,
-        permissions: { read: true, write: true },
-      });
+  {
+    const { exists, hasPermissions } = await accessPath({
+      path: DB_PATH,
+      permissions: { read: true, write: true },
+    });
+    if (exists && !hasPermissions) throwNotAccessibleError(DB_PATH);
 
-      if (exists && !hasPermissions) throwNotAccessibleError(DB_PATH);
-      if (exists) return;
-    }
-
-    await log("The database file does not exist. Looking for backup.");
-
-    {
+    if (!exists) {
+      await log("\tThe database file does not exist. Looking for backup.");
       const { exists, hasPermissions } = await accessPath({
         path: DB_BACKUP_PATH,
         permissions: { read: true, write: true },
@@ -55,18 +109,17 @@ export async function configureApplication(arg: configureApplication_Argument) {
 
       if (exists && !hasPermissions) throwNotAccessibleError(DB_BACKUP_PATH);
       if (exists) {
-        await log("Backup found. Trying to restore backup.");
+        await log("\tBackup found. Trying to restore backup.");
         await fsp.copyFile(DB_BACKUP_PATH, DB_PATH);
-        return;
+        await log("\tRestored backup.");
+      } else {
+        await log("\tNo backup found. Creating new database file.");
+        await fsp.writeFile(DB_PATH, "");
       }
-
-      await log("No backup found. Creating new database file.");
-      await fsp.writeFile(DB_PATH, "");
+    } else {
+      await log("\tDatabase file found.");
     }
-  })();
-
-  modifyConfig({ changes: { DB_PATH, DB_CLOSE_TIMEOUT_WHEN_KILLING: 1000 } });
-  // @TODO load config
+  }
 }
 
 function throwNotAccessibleError(path: string) {
