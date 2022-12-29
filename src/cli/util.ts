63b28dbcede3996a8d0d1b504268b7d301efe5a2
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-29 16:45:03 +0600

    feat(cli/util): add dateParser and getClient util

--- src/cli/util.ts
+++ src/cli/util.ts
@@ -1,6 +1,7 @@
 import colors from "ansi-colors";
 import { Client } from "express-ipc";
 import { InvalidArgumentError } from "commander";
+import { assertValidUSLocaleDateString } from "common/util/date-time";
 import { API_AND_SERVER_CONFIG as config, CLI_NAME } from "src/config/other";
 
 export function durationParser(value: any) {
@@ -37,5 +38,54 @@ export async function isServerRunning() {
     return false;
   } finally {
     if (client!) client.close();
+  }
+}
+
+export function dateStringParser(dateString: string) {
+  try {
+    assertValidUSLocaleDateString(dateString);
+  } catch (ex) {
+    throw new InvalidArgumentError(ex.message);
+  }
+
+  return new Date(dateString).valueOf();
+}
+
+export async function getClient(): Promise<Client> {
+  let client: Client;
+  try {
+    client = new Client({
+      path: {
+        id: config.SERVER_ID,
+        namespace: config.SERVER_NAMESPACE,
+      },
+    });
+
+    client.on("error", (error) => {
+      const message = error.message || "An unexpected error ocurred.";
+      console.log(formatStr({ color: "red", string: message }));
+
+      client.close();
+      process.exit(1);
+    });
+
+    await client.post(config.API_APP_PATH, {
+      query: {},
+      headers: {},
+      body: { name: "ping" },
+    });
+
+    return client;
+  } catch (ex) {
+    client!.close();
+
+    console.log(colors.red("Could not connect to server."));
+    console.log("Is the server running?");
+
+    const startCommand = `${CLI_NAME} bootup`;
+    console.log(`Use '${colors.inverse(startCommand)}' to start the server.`);
+
+    console.log("Error:", ex.message);
+    process.exit(1);
   }
 }
