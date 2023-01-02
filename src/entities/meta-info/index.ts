62e937177bcb09f3a2b53d20ccdafd91b0da9e1d
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2023-01-02 12:10:28 +0600

    feat(entities): add meta information entity

---
+++ src/entities/meta-info/index.ts
@@ -0,0 +1,50 @@
+import { z } from "zod";
+import EPP from "common/util/epp";
+import { createMD5Hash } from "common/util/other";
+import { formatError } from "common/validator/zod";
+
+export interface MetaInformationInterface {
+  lastBackupTime: number | null;
+  dailyWorkTargetMs: number | null;
+}
+
+const MetaInformationSchema = z
+  .object({
+    lastBackupTime: z
+      .union([z.number().positive().int(), z.null()])
+      .default(null),
+    dailyWorkTargetMs: z
+      .union([z.number().positive().int(), z.null()])
+      .default(null),
+  })
+  .strict();
+
+export function validateMetaInformation(
+  metaInfo: unknown,
+  hash?: string
+): asserts metaInfo is MetaInformationInterface {
+  const result = MetaInformationSchema.safeParse(metaInfo);
+  if (!result.success)
+    throw new EPP({
+      code: "INVALID_META_INFO",
+      message: formatError(result.error),
+    });
+
+  if (
+    typeof hash === "string" &&
+    hash !== generateMetaInfoHash(metaInfo as any)
+  )
+    throw new EPP({
+      code: "INVALID_META_INFO:INVALID_HASH",
+      message: `The metaInfo object is invalid because the hash doesn't match.`,
+    });
+}
+
+export function generateMetaInfoHash(metaInfo: MetaInformationInterface) {
+  const serialized = Object.entries(metaInfo)
+    .sort(([kA], [kB]) => kA.localeCompare(kB))
+    .map(([k, v]) => k + String(v))
+    .join("");
+
+  return createMD5Hash(serialized);
+}
---
+++ tests/src/entities/meta-info/index.test.ts
@@ -0,0 +1,28 @@
+import { validateMetaInformation } from "entities/meta-info";
+
+describe("ValidateMetaInformation", () => {
+  it.each([
+    {
+      hash: undefined,
+      case: "contains unknown props",
+      errorCode: "INVALID_META_INFO",
+      metaInfo: { unknownProps: "hi" },
+    },
+    {
+      metaInfo: {},
+      hash: "invalid-hash",
+      case: "hash doesn't match",
+      errorCode: "INVALID_META_INFO:INVALID_HASH",
+    },
+  ])(
+    `throws ewc "$errorCode" if metaInfo $case`,
+    ({ metaInfo, hash, errorCode }) => {
+      expect.assertions(1);
+      try {
+        validateMetaInformation(metaInfo, hash);
+      } catch (ex) {
+        expect(ex.code).toBe(errorCode);
+      }
+    }
+  );
+});
