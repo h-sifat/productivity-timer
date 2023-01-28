b341542d2367ed3ebeb03c52f54bae3a4310b2ef
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2023-01-28 08:34:33 +0600

    feat(client/services): add category service

---
+++ src/client/services/category.ts
@@ -0,0 +1,91 @@
+import {
+  CategoryFields,
+  MakeCategory_Argument,
+} from "entities/category/category";
+import type { Client } from "express-ipc";
+import type { Writable } from "type-fest";
+import { EditCategory_Argument } from "use-cases/interfaces/category-service";
+
+type CategoryInterface = Writable<CategoryFields>;
+
+export type GetAllCategoriesQuery = { lookup: "all" };
+export type GetCategoryByIdQuery = { lookup: "selfById"; id: string };
+export type GetParentCategoriesQuery = { lookup: "parents"; id: string };
+export type GetCategoryByNameQuery = { lookup: "selfByName"; name: string };
+export type GetChildrenCategoriesQuery = { lookup: "children"; id: string };
+
+export type GetCategoryQuery =
+  | GetCategoryByIdQuery
+  | GetParentCategoriesQuery
+  | GetAllCategoriesQuery
+  | GetCategoryByNameQuery
+  | GetChildrenCategoriesQuery;
+
+export interface CategoryService_Argument {
+  url: string;
+  client: Client;
+}
+
+export default class CategoryService {
+  #client: Client;
+  readonly #url: string;
+
+  constructor(arg: CategoryService_Argument) {
+    this.#client = arg.client;
+    this.#url = arg.url;
+  }
+
+  async findAll(): Promise<CategoryInterface[]> {
+    return this.#get({ lookup: "all" });
+  }
+  async findById(arg: { id: string }): Promise<CategoryInterface[]> {
+    return this.#get({ lookup: "selfById", id: arg.id });
+  }
+  async findByName(arg: { name: string }): Promise<CategoryInterface[]> {
+    return this.#get({ lookup: "selfByName", name: arg.name });
+  }
+  async findParents(arg: { id: string }): Promise<CategoryInterface[]> {
+    return this.#get({ lookup: "parents", id: arg.id });
+  }
+  async findChildren(arg: { id: string }): Promise<CategoryInterface[]> {
+    return this.#get({ lookup: "children", id: arg.id });
+  }
+
+  async add(category: MakeCategory_Argument) {
+    const { body } = (await this.#client.request({
+      body: category,
+      method: "post",
+      url: this.#url,
+    })) as any;
+
+    if (!body.success) throw body.error;
+    return body.data;
+  }
+
+  async edit(arg: EditCategory_Argument) {
+    const { id, changes } = arg;
+    const { body } = (await this.#client.request({
+      url: this.#url,
+      method: "patch",
+      body: { id, changes },
+    })) as any;
+
+    if (!body.success) throw body.error;
+    return body.data;
+  }
+
+  async #get(query: GetCategoryQuery): Promise<CategoryInterface[]> {
+    const { body } = (await this.#client.request({
+      query,
+      method: "get",
+      url: this.#url,
+    })) as any;
+
+    if (!body.success) throw body.error;
+    return body.data;
+  }
+
+  set client(client: Client) {
+    this.#client = client;
+  }
+}
---
+++ tests/src/client/services/category.test.ts
@@ -0,0 +1,127 @@
+import { Client } from "express-ipc";
+import { deepFreeze } from "common/util/other";
+import CategoryService from "client/services/category";
+
+const url = "/category";
+const client = Object.freeze({
+  request: jest.fn(),
+});
+
+const validArgs: { [k: string]: any } = deepFreeze({
+  add: { name: "a" },
+  findAll: undefined,
+  findById: { id: "a" },
+  findParents: { id: "a" },
+  findChildren: { id: "a" },
+  findByName: { name: "a" },
+  edit: { id: "a", changes: { name: "b" } },
+});
+
+let categoryService: CategoryService;
+beforeEach(() => {
+  Object.values(client).forEach((fn) => fn.mockReset());
+  categoryService = new CategoryService({
+    url,
+    client: client as unknown as Client,
+  });
+});
+
+describe("Error Handling", () => {
+  it.each(Object.keys(validArgs))(
+    `%p method throws the body.error if body.success is false`,
+    async (method) => {
+      expect.assertions(2);
+
+      const fakeResponse = deepFreeze({
+        body: {
+          success: false,
+          error: { message: "hmmm, interesting!", code: "err" },
+        },
+      });
+      client.request.mockResolvedValueOnce(fakeResponse);
+
+      try {
+        await (<any>categoryService)[method]((<any>validArgs)[method]);
+      } catch (ex) {
+        expect(ex).toEqual(fakeResponse.body.error);
+      }
+
+      expect(client.request).toHaveBeenCalledTimes(1);
+    }
+  );
+});
+
+describe("Functionality", () => {
+  it.each([
+    {
+      serviceMethod: "findAll",
+      arg: undefined,
+      requestArg: { method: "get", url, query: { lookup: "all" } },
+    },
+    {
+      serviceMethod: "findById",
+      arg: { id: "a" },
+      requestArg: {
+        method: "get",
+        url,
+        query: { lookup: "selfById", id: "a" },
+      },
+    },
+
+    {
+      serviceMethod: "findParents",
+      arg: { id: "a" },
+      requestArg: {
+        method: "get",
+        url,
+        query: { lookup: "parents", id: "a" },
+      },
+    },
+    {
+      serviceMethod: "findChildren",
+      arg: { id: "a" },
+      requestArg: {
+        method: "get",
+        url,
+        query: { lookup: "children", id: "a" },
+      },
+    },
+    {
+      serviceMethod: "findByName",
+      arg: { name: "a" },
+      requestArg: {
+        method: "get",
+        url,
+        query: { lookup: "selfByName", name: "a" },
+      },
+    },
+    {
+      serviceMethod: "add",
+      arg: { name: "a" },
+      requestArg: { method: "post", url, body: { name: "a" } },
+    },
+    {
+      serviceMethod: "edit",
+      arg: { id: "1", changes: { name: "a" } },
+      requestArg: {
+        url,
+        method: "patch",
+        body: { id: "1", changes: { name: "a" } },
+      },
+    },
+  ])(
+    `the Service.$serviceMethod issues a "$requestArg.method" method and returns body.data if body.success is true`,
+    async ({ serviceMethod, arg, requestArg }) => {
+      const fakeResponse = deepFreeze({
+        body: { success: true, data: { a: 1 } },
+      });
+      client.request.mockResolvedValueOnce(fakeResponse);
+
+      const data = await (<any>categoryService)[serviceMethod](arg);
+
+      expect(data).toEqual(fakeResponse.body.data);
+      expect(client.request).toHaveBeenCalledTimes(1);
+      expect(client.request).toHaveBeenCalledWith(requestArg);
+    }
+  );
+});
