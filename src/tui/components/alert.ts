200e46a6fdbc95246a95e2d4970448fec4926b83
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2023-01-29 21:40:26 +0600

    feat(tui/components): add alert element

---
+++ src/tui/components/alert.ts
@@ -0,0 +1,39 @@
+import blessed, { Widgets } from "blessed";
+
+export type Alert = (
+  arg: string | { text: string; type: "error" | "log" }
+) => void;
+
+export function createAlertElement(): {
+  element: Widgets.MessageElement;
+  alert: Alert;
+} {
+  const alertBox = blessed.message({
+    label: "[Alert]",
+
+    width: 44,
+    height: 8,
+
+    right: 0,
+    bottom: 0,
+
+    mouse: true,
+    align: "center",
+    scrollable: true,
+
+    tags: true,
+    border: "line",
+    style: { focus: { border: { fg: "green" } }, label: { fg: "green" } },
+  });
+
+  alertBox.hide();
+
+  const alert: Alert = function _alert(arg) {
+    const { text, type } =
+      typeof arg === "string" ? ({ text: arg, type: "log" } as const) : arg;
+
+    alertBox[type](text, () => {});
+  };
+
+  return { element: alertBox, alert };
+}
