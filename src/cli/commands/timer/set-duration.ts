8ffd9d3dbd6747c9875a171a291df9d437cdf640
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2023-03-08 12:23:00 +0600

    feat(cli/timer/set-duration): allow option to increment and decrement

--- src/cli/commands/timer/set-duration.ts
+++ src/cli/commands/timer/set-duration.ts
@@ -5,6 +5,8 @@ import {
 } from "cli/util/timer";
 
 import type { Command } from "commander";
+
+import { Option } from "commander";
 import { withClient } from "cli/util/client";
 import { TimerService } from "client/services/timer";
 import { API_AND_SERVER_CONFIG as config } from "src/config/other";
@@ -13,18 +15,33 @@ export function addSetTimerDurationCommand(program: Command) {
   program
     .command("set-duration")
     .description("Changes the timer duration while the timer is not running.")
+    .addOption(
+      new Option("-i, --increment", "increment duration").conflicts("decrement")
+    )
+    .addOption(
+      new Option("-d, --decrement", "increment duration").conflicts("increment")
+    )
     .argument("<duration>", durationOptionDescription, parseCliDurationArg)
     .action(setDuration);
 }
 
-async function setDuration(duration: number) {
+type SetDurationOptions = { increment?: true } | { decrement?: true };
+
+async function setDuration(duration: number, options: SetDurationOptions) {
   await withClient(async (client) => {
     const timerService = new TimerService({
       client,
       url: config.API_TIMER_PATH,
     });
 
-    const data = await timerService.setDuration({ duration });
+    let changeType = "absolute";
+    if ("increment" in options) changeType = "increment";
+    else if ("decrement" in options) changeType = "decrement";
+
+    const data = await timerService.setDuration({
+      duration,
+      changeType: <any>changeType,
+    });
     printTimerMethodCallResult(data);
   });
 }
