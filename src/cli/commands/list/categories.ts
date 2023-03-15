977456b26892b031d5d0c9408afcdbf1548ea14f
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2023-03-15 10:59:22 +0600

    refactor(cli/commands/list/category): reuse tree printer util

--- src/cli/commands/list/categories.ts
+++ src/cli/commands/list/categories.ts
@@ -1,9 +1,10 @@
 import { Option } from "commander";
 import type { Command } from "commander";
 import { withClient } from "cli/util/client";
-import { printTree } from "flexible-tree-printer";
 import { formatString, printObjectAsBox } from "cli/util";
 import { CategoryFields } from "entities/category/category";
+import { getTreeLines } from "tui/pages/category/get-lines";
+import { buildCategoryTree } from "entities/category/build-tree";
 import { API_AND_SERVER_CONFIG as config } from "src/config/other";
 import { preprocessCategory, printCategoriesAsTable } from "cli/util/category";
 import type { QuerySchemaInterface } from "src/controllers/category/get-categories";
@@ -86,69 +87,13 @@ function makeQueryFromOptions(
 }
 
 function printCategoriesAsTree(categories: CategoryFields[]) {
-  const tree = buildTree(categories);
-  printTree({
-    printRootNode: () =>
-      console.log(formatString({ string: ".", color: "yellow" })),
-    parentNode: tree,
-    getSubNodes: ({ parentNode }) =>
-      parentNode!.children.map((child: any) => ({
-        name: child.name,
-        value: child,
-      })),
-    printNode({ nodePrefix, node }) {
-      const name = formatString({ string: node.name, color: "green" });
-      const prefix = formatString({
-        string: nodePrefix.join(""),
-        color: "yellow",
-      });
-      const id = formatString({ color: "grey", string: `(${node.value.id})` });
-      const line = `${prefix}${name} ${id}`;
-      console.log(line);
-    },
-  });
-}
+  const tree = buildCategoryTree(categories);
+  const lines = getTreeLines({ rootBranch: tree });
 
-type Branch = CategoryFields & { children: Branch[]; isRoot: boolean };
-
-interface Tree {
-  [key: string | symbol]: Branch;
-}
-
-function buildTree(categories: CategoryFields[]) {
-  const tree: Tree = {};
-  const DEFAULT_ROOT_ID = Symbol();
-
-  for (const category of categories) {
-    const currentBranch: Branch = {
-      ...category,
-      isRoot: false,
-      children: [] as Branch[],
-    };
-
-    if (category.id in tree) {
-      currentBranch.children = tree[category.id]!.children;
-      tree[category.id] = currentBranch;
-    } else tree[category.id] = currentBranch;
-
-    const parentId = category.parentId || DEFAULT_ROOT_ID;
-
-    if (!tree[parentId]) {
-      const parentBranch: Branch = {
-        name: "",
-        children: [],
-        // @ts-ignore I know
-        id: parentId,
-        isRoot: true,
-      };
-
-      tree[parentId] = parentBranch;
-    }
-
-    tree[parentId]!.children.push(currentBranch);
-  }
-
-  if (tree[DEFAULT_ROOT_ID]) return tree[DEFAULT_ROOT_ID];
-  for (const branch of Object.values(tree)) if (branch.isRoot) return branch;
-  throw new Error("Parent not found");
+  const treeText = lines
+    .map(({ line, id }) =>
+      id ? line + formatString({ string: ` (${id})`, color: "grey" }) : line
+    )
+    .join("\n");
+  console.log(treeText);
 }
